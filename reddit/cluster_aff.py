dbd3dbc0057d6a5a42c5cf06d35405066a7eca13
Scott Carr <s.carr1024@gmail.com>
2010-11-13 21:57:58 -0500

    improved accuracy algo

--- reddit/cluster_aff.py
+++ reddit/cluster_aff.py
@@ -1,5 +1,7 @@
 import csv
 import pdb
+from operator import itemgetter
+import heapq
 
 def sum_cluster_affinities(memberships, aff_filename):
 	#takes the user_id, cluster_id pairs from memberships
@@ -32,45 +34,90 @@ def sum_cluster_affinities(memberships, aff_filename):
 					clusters[c_id] = { sr_id : affinity } 
 		except KeyError:
 			#apparently not all UIDs in clabel are in the output of srrecs.r	 
-			print user_id, "in affinities.clabel but not in srrecs.r output"
+			#print user_id, "in affinities.clabel but not in srrecs.r output"
+                        pass
 	f_aff.close()
 	return clusters
 
-def find_affs_in_cluster(members, top_srs, aff_filename, threshold):		
+def find_affs_in_cluster(memberships, top_srs, aff_filename, threshold):		
 	f_aff = open(aff_filename)
 	aff_reader = csv.reader(f_aff,delimiter='\t')
 	
 	total_good = 0
 	total_bad = 0		
+        totals = [0,0]
 	
-	for row in aff_reader:
+        for row in aff_reader:
 		#each for in aff file is a tuple, so need to get the parts
 		user_id = row[0]
 		sr_id = row[1]
 		affinity = float(row[2])
 		
-		try:
-			if members[user_id]['belongs'] == 1:
-			
-				#pdb.set_trace()
-				good = 0
-				bad = 0
-	
-				for sr in top_srs:
-					#pdb.set_trace()
-					if sr[0] == sr_id:
-						#print affinity, threshold
-						if affinity > threshold:
-							members[user_id]['good'] += 1
-							total_good += 1
-						else:	
-							members[user_id]['bad'] += 1
-							total_bad += 1
-		except KeyError:
-			#do nothing
-			x = 1
+                try:
+                        cluster = int(memberships[user_id])
+                        if sr_id in top_srs[cluster]:
+                                if affinity > threshold:
+                                       total_good += 1
+                                else:
+                                        total_bad += 1
+                except KeyError:
+                        #notall UIDs in affinities.dump appear in affinities.clabel
+                        pass
+		#try:
+                #        if user_id in members:
+                #                for sr in top_srs:
+                #                        #pdb.set_trace()
+                #                        if sr[0] == sr_id:
+                #                                #print affinity, threshold
+                #                                if affinity > threshold:
+                #                                        members[user_id]['good'] += 1
+                #                                        total_good += 1
+                #                                else:	
+                #                                        members[user_id]['bad'] += 1
+                #                                        total_bad += 1
+		#except KeyError:
+		#	#do nothing
+		#	print "user not found"
 
-	for user in members:
-		print user, members[user]['good'], "good and ", members[user]['bad'], "bad"	
+	#for user in members:
+	#	print user, members[user]['good'], "good and ", members[user]['bad'], "bad"	
 	
-	print "Totals:", total_good,"good and", total_bad, "bad"
+        #print "Total for cluster",cluster_no,':', total_good,"good and", total_bad, "bad"
+        totals[0] = total_good
+        totals[1] = total_bad
+        return totals
+
+def test_clusters(memberships, filename, threshold, clusters, n_top):
+        good = 0
+        bad = 0
+        largest = range(0,len(clusters)+1)
+        for i in range(1,len(clusters)+1):
+                temp_largest = heapq.nlargest(n_top, clusters[i].iteritems(),itemgetter(1))
+                largest[i] = {}
+                for item in temp_largest:
+                        largest[i][item[0]] =  item[1]
+                
+                #cluster_users = {}
+                #gives a dict of (key)user_id, (value) [good, bad]
+                #clust_users = {}
+                #for user_id in memberships:
+                 #       cluster_id = int(memberships[user_id])
+                  #      if cluster_id == i:
+                   #             cluster_users[user_id] = { 'good' : 0, 'bad' : 0 }		
+
+        totals = find_affs_in_cluster(memberships, largest, filename,threshold)
+        good += totals[0]
+        bad += totals[1]
+
+        print "Total good recommendations:", good
+        print "Total bad recommendations:", bad
+        print "Percent good:", float( good*100.00 / (bad + good) )
+  
+        
+
+
+
+
+
+
+
--- reddit/test.py
+++ reddit/test.py
@@ -1,27 +1,17 @@
 import pdb
-import heapq
 import membership
 import cluster_aff
-from operator import itemgetter
 
 N_LARGEST = 10
 ACC_THRESHOLD = 0.5
+N_CLUSTERS = 50
 
+# memberships is a dict, i.e. memberships['user_id'] = cluster_to which_the_user_belongs
 memberships = membership.build_membership('affinities.clabel','clusters')
 
+# cluster is a dict where the keys are cluster ids 1...50 and the values are a list of users that
+# belong to that cluster
 clusters = cluster_aff.sum_cluster_affinities(memberships, 'my_publicvotes-20101018_affinities.dump')
 
-largest = heapq.nlargest(N_LARGEST, clusters[1].iteritems(),itemgetter(1))
-
-#this chunk will give all the UIDs of a particular cluster
-#need to figure out a way of testing if the members of the cluster really
-# like the most popular subreddits of that cluster
-cluster_members = {}
-c_id = '1'
-for c_member in memberships:
-	if memberships[c_member] == c_id:
-		cluster_members[c_member] = { 'good' : 0, 'bad' : 0, 'belongs' : 1 }
-		
-
-cluster_aff.find_affs_in_cluster(cluster_members, largest, 'my_publicvotes-20101018_affinities.dump',ACC_THRESHOLD)
-
+#this does the actual testing and printing
+cluster_aff.test_clusters(memberships,'my_publicvotes-20101018_affinities.dump', ACC_THRESHOLD, clusters, N_LARGEST)
