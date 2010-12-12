84d059632d845daf31a546fbb5ddee096013b76a
Scott Carr <s.carr1024@gmail.com>
2010-12-12 12:49:14 -0500

    added networkx beginings

--- ece570project3/project3.py
+++ ece570project3/project3.py
@@ -3,6 +3,7 @@ import Image
 import random
 import matplotlib.pyplot as plt
 import pdb
+import networkx as nx
 
 def find_zeros_and_ones(mylist):
     my_zeros = []
@@ -36,6 +37,15 @@ def transform_indexes(index_list, cols):
     return x_coords, y_coords
         
 
+def return_vehicles_in_range(this_vehicle,vehicles, s_range):
+
+	vehicles_in_range = []
+	for vehicle in vehicles:
+		if (vehicle[0] <= this_vehicle[0] + s_range / 2) and (vehicle[0] >=this_vehicle[0] - s_range / 2):
+			if (vehicle[1] <= this_vehicle[1] + s_range / 2) and (vehicle[1] >= this_vehicle[1] - s_range / 2):
+ 				vehicles_in_range.append(vehicle)
+	return vehicles_in_range
+
 Nv=200 #Number of vehicles
 Lsx=100
 Lsy=100
@@ -63,6 +73,40 @@ for pair in vehicles:
 	vehicles_x.append(pair[0])
 	vehicles_y.append(pair[1])
 
+
+G = nx.Graph()
+
+for vehicle in vehicles:
+	v_x = vehicle[0]
+	v_y = vehicle[1]
+	name = str(v_x)
+	name += str(v_y)
+	G.add_node(name)
+	G.node[name]['x'] = v_x
+	G.node[name]['y'] = v_y
+	G.nodes(data=True)
+
+s_range = 40
+
+G.add_node('source', x = Lsx, y = Lsy)
+G.add_node('destination', x = Ldx, y = Ldy)
+
+for node in vehicles:
+	for other_node in vehicles:
+		if (node[0] >= other_node[0] - s_range/2) and (node[0] <= other_node[0] + s_range/2) and (node[1] >= other_node[1] - s_range/2) and (node[1] <= other_node[1] + s_range/2):
+			name1 = node
+			name1 = str(node[0])
+			name1 += str(node[1])
+			name2 = other_node
+			name2 = str(other_node[0])
+			name2 += str(other_node[1])
+			if name1 != name2:
+				G.add_edge(name1, name2)
+
+#need to add code to add source and destination edges to graph
+
+pdb.set_trace()
+
 plt.scatter(vehicles_x, vehicles_y, marker = 'o')
 plt.scatter(x_coords, y_coords, s = 1)
 plt.scatter(Lsx, Lsy, marker = '^', color = 'r')
@@ -70,16 +114,6 @@ plt.scatter(Ldx, Ldy, marker = '^', color = 'r')
 plt.annotate('source', (Lsx, Lsy))
 plt.annotate('destination', (Ldx, Ldy))
 plt.show()
-
-#Generate randon distribution of vehicles.
-
-
-
-
-
-
-xs = []
-ys = []
 
 
 '''%change color for demonstration
